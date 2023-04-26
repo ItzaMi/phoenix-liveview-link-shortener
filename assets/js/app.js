2da82218846f31d79ed6006d8688cad409425b40
Rui Sousa <30603437+ItzaMi@users.noreply.github.com>
2023-04-26 16:14:00 +0100

    fix(app.js): remove `Hooks`

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,16 +22,11 @@ import { Socket } from 'phoenix';
 import { LiveSocket } from 'phoenix_live_view';
 import topbar from '../vendor/topbar';
 
-import MessageToaster from './message_toaster';
-
-let Hooks = { MessageToaster };
-
 let csrfToken = document
   .querySelector("meta[name='csrf-token']")
   .getAttribute('content');
 let liveSocket = new LiveSocket('/live', Socket, {
   params: { _csrf_token: csrfToken },
-  hooks: Hooks,
 });
 
 // Show progress bar on live navigation and form submits
